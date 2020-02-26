8f9a93fd0ad31756bbee58d2ecef89fd37237dfb
Jan Kuehle <jkuehle90@gmail.com>
2020-02-26 20:28:58 +0000

    Use UTF8 encoding for base64 function

--- aliases/base64.ps1
+++ aliases/base64.ps1
@@ -2,10 +2,10 @@ function base64([switch]$D)
 {
     if ($D)
     {
-        [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($input))
+        [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($input))
     }
     else
     {
-        [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($input))
+        [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($input))
     }
 }
