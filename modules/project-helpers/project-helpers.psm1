7462fca5dbf3eac9d80d92e18b264feac408a188
Jan Kuehle <jkuehle90@gmail.com>
2020-04-21 21:59:18 +0100

    Use Register-ArgumentCompleter for tab completion

--- modules/project-helpers/project-helpers.psm1
+++ modules/project-helpers/project-helpers.psm1
@@ -1,7 +1,12 @@
 <#
 .SYNOPSIS
-    Helper for working with JavaScript projects from the command line.
+	Helper for working with JavaScript projects from the command line.
 #>
+
+
+
+using namespace System.Management.Automation
+using namespace System.Management.Automation.Language
 
 
 
@@ -9,7 +14,7 @@
 
 function Get-ProjectPath([string]$ProjectName)
 {
-    return Join-Path -Path $PROJECTS_DIRECTORY -ChildPath $ProjectName
+	return Join-Path -Path $PROJECTS_DIRECTORY -ChildPath $ProjectName
 }
 
 #endregion
@@ -19,131 +24,125 @@ function Get-ProjectPath([string]$ProjectName)
 #region Exported Cmdlets
 
 function Set-ProjectDirectory {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position=0, Mandatory=$true)]
-        [string] $Directory
-    )
-    process
-    {
-        Set-Variable -Name PROJECTS_DIRECTORY -Value $Directory -Option Constant -Scope Script
-    }
+	[CmdletBinding()]
+	param (
+		[Parameter(Position=0, Mandatory=$true)]
+		[string] $Directory
+	)
+	process
+	{
+		Set-Variable -Name PROJECTS_DIRECTORY -Value $Directory -Option Constant -Scope Script
+	}
 }
 
 function Set-VisualStudioPath {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position=0, Mandatory=$true)]
-        [string] $Path
-    )
-    process
-    {
-        Set-Variable -Name VISUAL_STUDIO_PATH -Value $Path -Option Constant -Scope Script
-    }
+	[CmdletBinding()]
+	param (
+		[Parameter(Position=0, Mandatory=$true)]
+		[string] $Path
+	)
+	process
+	{
+		Set-Variable -Name VISUAL_STUDIO_PATH -Value $Path -Option Constant -Scope Script
+	}
 }
 
 function Set-EditorPath {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position=0, Mandatory=$true)]
-        [string] $Path
-    )
-    process
-    {
-        Set-Variable -Name EDITOR_PATH -Value $Path -Option Constant -Scope Script
-    }
+	[CmdletBinding()]
+	param (
+		[Parameter(Position=0, Mandatory=$true)]
+		[string] $Path
+	)
+	process
+	{
+		Set-Variable -Name EDITOR_PATH -Value $Path -Option Constant -Scope Script
+	}
 }
 
 function Set-Project {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position=0, Mandatory=$true)]
-        [string] $ProjectName
-    )
-    process
-    {
-        cd (Get-ProjectPath $ProjectName)
-    }
+	[CmdletBinding()]
+	param (
+		[Parameter(Position=0, Mandatory=$true)]
+		[string] $ProjectName
+	)
+	process
+	{
+		cd (Get-ProjectPath $ProjectName)
+	}
 }
 
 function Invoke-NpmStart {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position=0, Mandatory=$false)]
-        [string] $ProjectName
-    )
-    process
-    {
-        if ($ProjectName)
-        {
-            Set-Project -ProjectName $ProjectName
-        }
+	[CmdletBinding()]
+	param (
+		[Parameter(Position=0, Mandatory=$false)]
+		[string] $ProjectName
+	)
+	process
+	{
+		if ($ProjectName)
+		{
+			Set-Project -ProjectName $ProjectName
+		}
 
-        npm start
-    }
+		npm start
+	}
 }
 
 function Invoke-YarnStart {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position=0, Mandatory=$false)]
-        [string] $ProjectName
-    )
-    process
-    {
-        if ($ProjectName)
-        {
-            Set-Project -ProjectName $ProjectName
-        }
+	[CmdletBinding()]
+	param (
+		[Parameter(Position=0, Mandatory=$false)]
+		[string] $ProjectName
+	)
+	process
+	{
+		if ($ProjectName)
+		{
+			Set-Project -ProjectName $ProjectName
+		}
 
-        yarn start
-    }
+		yarn start
+	}
 }
 
 function Invoke-EditorForProject {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position=0, Mandatory=$false)]
-        [string] $ProjectName
-    )
-    process
-    {
-        $path = "."
-        if ($ProjectName)
-        {
-            $path = Get-ProjectPath $ProjectName
-        }
+	[CmdletBinding()]
+	param (
+		[Parameter(Position=0, Mandatory=$false)]
+		[string] $ProjectName
+	)
+	process
+	{
+		$path = "."
+		if ($ProjectName)
+		{
+			$path = Get-ProjectPath $ProjectName
+		}
 
-        $projects = @()
-        $projects += ls $path\*.sublime-project | %{ $_.FullName }
-        $projects += ls $path\*.sln, $path\*\*.sln | %{ $_.FullName }
-        $projects += (Get-Item $path).FullName
+		$projects = @()
+		$projects += ls $path\*.sln, $path\*\*.sln | %{ $_.FullName }
+		$projects += (Get-Item $path).FullName
 
-        $project = $projects[0]
-        if ($projects.Count -gt 2)
-        {
-            $project = $projects | Out-GridView -OutputMode Single
-        }
+		$project = $projects[0]
+		if ($projects.Count -gt 2)
+		{
+			$project = $projects | Out-GridView -OutputMode Single
+		}
 
-        if ($project)
-        {
-            if ($project.EndsWith(".sublime-project"))
-            {
-                "Opening project $project in Sublime Text"
-                subl --project $project
-            }
-            elseif ($project.EndsWith(".sln"))
-            {
-                "Opening project $project in Visual Studio"
-                . $VISUAL_STUDIO_PATH $project
-            }
-            else
-            {
-                "Opening path $project in Editor"
-                . $EDITOR_PATH $project
-            }
-        }
-    }
+		if ($project)
+		{
+			if ($project.EndsWith(".sln"))
+			{
+				"Opening project $project in Visual Studio"
+				. $VISUAL_STUDIO_PATH $project
+			}
+			else
+			{
+				"Opening path $project in Editor"
+				. $EDITOR_PATH $project
+			}
+		}
+	}
 }
 
 #endregion
@@ -152,57 +151,16 @@ function Invoke-EditorForProject {
 
 #region TabExpansion
 
-# Backup old tabexpansion
-$tabExpansionBackup = 'ProjectHelpers_DefaultTabExpansion'
-if ((Test-Path -Path Function:\TabExpansion -ErrorAction SilentlyContinue) -and -not (Test-Path -Path Function:\$tabExpansionBackup -ErrorAction SilentlyContinue))
-{
-    Rename-Item -Path Function:\TabExpansion $tabExpansionBackup -ErrorAction SilentlyContinue
+$completerScript = {
+	param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+	Get-ChildItem (Get-ProjectPath "$wordToComplete*") | ForEach-Object {
+		[CompletionResult]::new($_.Name,$_.Name, [CompletionResultType]::Variable,$_.Name)
+	}
 }
-
-# Revert old tabexpansion when module is unloaded
-$Module = $MyInvocation.MyCommand.ScriptBlock.Module
-$Module.OnRemove = {
-    Write-Debug 'Revert tab expansion back'
-    Remove-Item -Path Function:\TabExpansion -ErrorAction SilentlyContinue
-    if (Test-Path -Path Function:\$tabExpansionBackup)
-    {
-        Rename-Item -Path Function:\$tabExpansionBackup Function:\TabExpansion
-    }
-}
-
-function TabExpansion
-{
-    [CmdletBinding()]
-    param (
-        [String] $line,
-        [String] $lastWord
-    )
-    process
-    {
-        $commands = @(
-            "Set-ProjectDirectory",
-            "Set-Project",
-            "Invoke-NpmStart",
-            "Invoke-YarnStart",
-            "Invoke-EditorForProject",
-            "p",
-            "ns",
-            "ys",
-            "edit"
-        )
-
-        $words = $line.Split(" ")
-        $command = $words[0]
-        if ($words.Length -eq 2 -and $commands.Contains($command))
-        {
-            Get-ChildItem (Get-ProjectPath "$lastWord*") | %{ $_.Name } | sort -Unique
-        }
-        elseif (Test-Path -Path Function:\$tabExpansionBackup)
-        {
-            & $tabExpansionBackup $line $lastWord
-        }
-    }
-}
+Register-ArgumentCompleter -CommandName Set-Project -ParameterName ProjectName -ScriptBlock $completerScript
+Register-ArgumentCompleter -CommandName Invoke-NpmStart -ParameterName ProjectName -ScriptBlock $completerScript
+Register-ArgumentCompleter -CommandName Invoke-YarnStart -ParameterName ProjectName -ScriptBlock $completerScript
+Register-ArgumentCompleter -CommandName Invoke-EditorForProject -ParameterName ProjectName -ScriptBlock $completerScript
 
 #endregion
 
@@ -222,7 +180,6 @@ Export-ModuleMember Set-Project
 Export-ModuleMember Invoke-NpmStart
 Export-ModuleMember Invoke-YarnStart
 Export-ModuleMember Invoke-EditorForProject
-Export-ModuleMember TabExpansion
 Export-ModuleMember -Alias p
 Export-ModuleMember -Alias ns
 Export-ModuleMember -Alias ys
