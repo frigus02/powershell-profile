4f005f59b9f6642fdbe07a6177d7811d09b0e373
Jan Kuehle <jkuehle90@gmail.com>
2020-04-21 22:47:16 +0100

    Test for native ssh agent

--- profiles/default.ps1
+++ profiles/default.ps1
@@ -8,6 +8,22 @@ $Env:PSModulePath += ";$RepoPath\modules"
 Import-Module project-helpers
 Import-Module posh-git
 
+# Test SSH agent config
+$sshAgent = Get-Service ssh-agent -ErrorAction Ignore
+if ($sshAgent) {
+	if ($sshAgent.StartType -eq "Disabled" -or
+		$sshAgent.Status -ne "Running") {
+		Write-Host "The ssh-agent service is not running. Set the service to automatic and try again."
+	} else {
+		$sshCommand = (Get-Command ssh.exe -ErrorAction Ignore | Select-Object -ExpandProperty Path).Replace("\", "/")
+		$configuredSshCommand = git config --global core.sshCommand
+		if ($sshCommand -ne $configuredSshCommand) {
+			Write-Host "git is not configured to use the native ssh command. To fix run:"
+			Write-Host "    git config --global core.sshCommand $sshCommand"
+		}
+	}
+}
+
 . "$RepoPath\aliases\all.ps1"
 . "$RepoPath\aliases\base64.ps1"
 . "$RepoPath\aliases\clipboard.ps1"
